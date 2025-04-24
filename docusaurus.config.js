0ce1bbf152cef79628bec68f9755f9ca29d0c90a
Pukkok <krystalgggg@gmail.com>
2025-04-25 00:30:10 +0900

    editurl 변경

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,8 +41,7 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/pukkok/dev-note/edit/main/',
         },
         blog: {
           showReadingTime: true,
