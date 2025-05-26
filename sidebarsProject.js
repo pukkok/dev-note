16f8cd9f89ac8af541b396e616a2aeb02c03a829
Pukkok <krystalgggg@gmail.com>
2025-05-27 01:19:32 +0900

    [Feat] 이미지 뚝딱기 사이드바 연결

---
+++ sidebarsProject.js
@@ -0,0 +1,21 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+module.exports = {
+  sidebar: [
+    {
+      type: 'category',
+      label: '이미지 뚝딱기',
+      items: [
+        'img-ttukttakki/intro',
+        'img-ttukttakki/challenges',
+      ],
+    },
+    // {
+    //   type: 'category',
+    //   label: 'React Merge Table',
+    //   items: [
+    //     'react-merge-table/intro',
+    //     'react-merge-table/design',
+    //   ],
+    // },
+  ],
+};
