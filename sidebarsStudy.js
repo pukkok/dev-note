2a71525fa26f4d0262e7d36f07553dedb29147eb
Pukkok <krystalgggg@gmail.com>
2025-05-27 16:08:01 +0900

    [Fix] 스네이크로 전체 변경

--- sidebarsStudy.js
+++ sidebarsStudy.js
@@ -7,50 +7,56 @@ module.exports = {
       items: [
         'nextjs/routing',
         'nextjs/enviroment',
-        'nextjs/apiProxy',
+        'nextjs/api_proxy',
       ],
     },
+    
     {
       type: 'category',
-      label: 'github',
+      label: 'Javascript',
       items: [
-        'github/permissionError',
-        'github/started',
-        'github/advanced',
-        'github/githubPages404redirect',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'javascript',
-      items: [
-        'javascript/functionsIntro',
-        'javascript/functionsPerspective'
+        'javascript/functions_intro',
+        'javascript/functions_perspective',
+        'javascript/array_object_copy',
+        'javascript/object_essentials',
+        'javascript/constructor_functions',
+        'javascript/private_public_property',
+        'javascript/prototype_extends_instanceof',
       ]
     },
     {
       type: 'category',
-      label: 'node.js',
+      label: 'Github',
       items: [
-        'nodejs/fileGeneration',
+        'github/permission_error',
+        'github/started',
+        'github/advanced',
+        'github/github_pages_404_redirect',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Node.js',
+      items: [
+        'nodejs/file_generation',
         'nodejs/started',
       ]
     },
     {
       type: 'category',
-      label: 'python',
+      label: 'Python',
       items: [
         'python/install',
-        'python/runError-1',
+        'python/run_error_1',
         'python/venv',
-        'python/initPyAndCachePy',
+        'python/initpy_pycache',
           {
           type: 'category',
           label: 'restful API 구현',
           items: [
-            'python/restful/training-1',
-            'python/restful/training-2',
-            'python/restful/training-3',
+            'python/restful/training_1',
+            'python/restful/training_2',
+            'python/restful/training_3',
         ],
       },
       ],
@@ -59,7 +65,7 @@ module.exports = {
       type: 'category',
       label: 'etc',
       items: [
-        'etc/computerBasic',
+        'etc/computer_basic',
         'etc/IDE',
       ],
     },
